3b4c85f6f87c286acc0041f613441ecef7703a84
Mathan281101 <Mathan281101>
2023-12-13 06:13:36 +0000

    Pushed from Locofy Builder

---
+++ GlobalStyles.js
@@ -0,0 +1,29 @@
+/* fonts */
+export const FontFamily = {
+  notoSansRegular: "NotoSans-Regular",
+  interRegular: "Inter-Regular",
+  nunitoSans12ptRegular: "NunitoSans12pt-Regular",
+  interBold: "Inter-Bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_xs: 12,
+  size_lg: 18,
+  size_mini: 15,
+  size_2xs: 11,
+};
+/* Colors */
+export const Color = {
+  colorGhostwhite: "#f6faff",
+  colorDeepskyblue: "#08b3f1",
+  colorWhite: "#fff",
+  colorDarkslategray_100: "#393d46",
+  colorDarkslategray_200: "rgba(57, 61, 70, 0.5)",
+  colorBlack: "#000",
+  colorMediumseagreen: "#2ecc71",
+  colorGainsboro: "#d9d9d9",
+};
+/* border radiuses */
+export const Border = {
+  br_8xs: 5,
+};
